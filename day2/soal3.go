3087fc0dd9c8551baad407756c2786ffb413e7c9
abdisudiatmika <sudiatmika.abdi@gmail.com>
2019-12-05 19:27:21 +0700

    tambah soal problem 3

---
+++ day2/soal3.go
@@ -0,0 +1,31 @@
+package coba
+
+import "fmt"
+
+func prime(numbers ...int) int {
+	var faktor int
+	for i, numbers := range numbers {
+		if numbers%i == 0 {
+			faktor += faktor
+
+		} else {
+			faktor = faktor
+
+		}
+	}
+	if faktor == 2 {
+		fmt.Println(numbers)
+	} else {
+		fmt.Println(numbers)
+	}
+
+}
+
+// //GetPrima sdadadadadadasd
+// func GetPrima(X int)int {
+
+// 		for i := 1; i <= N; i++ {
+
+// 	}
+
+// }
